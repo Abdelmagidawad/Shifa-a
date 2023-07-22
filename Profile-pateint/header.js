dbbfa06eb6e48bed0060b8cc0760edfadd9d22fd
Abdelmagidawad <abdelmagidawad@gmail.com>
2023-07-23 01:13:08 +0300

    responsive links header doc and pationt and adde notifications and scrollbar

--- Profile-pateint/header.js
+++ Profile-pateint/header.js
@@ -1,3 +1,21 @@
+// Links Header
+let iconMenu = document.querySelector(".menu-links .icon");
+let pLinks = document.querySelector(".menu-links .links");
+
+iconMenu.addEventListener("click", () => {
+  iconMenu.firstElementChild.classList.toggle("anemy2");
+  iconMenu.firstElementChild.nextElementSibling.classList.toggle("anemy1");
+  iconMenu.lastElementChild.classList.toggle("anemy3");
+  pLinks.classList.toggle("visiblle");
+});
+
+let notificationIcon = document.querySelector(".note>i");
+let notifications = document.querySelector(".note .content-note");
+
+notificationIcon.addEventListener("click", () => {
+  notifications.classList.toggle("show");
+});
+
 let imagHeader = document.querySelector(".Profile-toltab img");
 let toolTip = document.querySelector(".Profile-toltab .tol-tab");
 
