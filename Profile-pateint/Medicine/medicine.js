c40792faf8568d269fe319711049a164874e29e2
Abdelmagidawad <abdelmagidawad@gmail.com>
2023-12-11 20:45:13 +0200

    add popup details medicine to patient Medicines

--- Profile-pateint/Medicine/medicine.js
+++ Profile-pateint/Medicine/medicine.js
@@ -1,3 +1,4 @@
+//Select Day
 let selectDay = document.querySelectorAll(".select-day div");
 let secNoMedicne = document.querySelector(".no-medicine");
 let secMedicne = document.querySelector(".sec-medicine");
@@ -25,4 +26,36 @@ selectDay[3].addEventListener("click", function () {
 selectDay[5].addEventListener("click", function () {
   secNoMedicne.style.display = "block";
   secMedicne.style.display = "none";
+});
+
+// when Click Medicine Show  Details
+let boxMedicine = document.querySelectorAll(".sec-medicine .box-med");
+let parentContentMedicine = document.querySelector(".popup-details");
+let closeButton = document.querySelector("#close i");
+let overlay = document.querySelector(".details-up .D-overlay");
+
+// content PopUp details medicine dynamic
+let imageMedicinePop = document.querySelector(".popup-details img");
+let nameMedicinePop = document.querySelector(".details #name span");
+//
+boxMedicine.forEach(function (ele) {
+  ele.addEventListener("click", (e) => {
+    parentContentMedicine.parentElement.classList.toggle("active");
+    //
+    imageMedicinePop.setAttribute(
+      "src",
+      e.currentTarget.firstElementChild.getAttribute("src")
+    );
+    nameMedicinePop.innerHTML =
+      e.currentTarget.lastElementChild.firstElementChild.textContent;
+    //
+  });
+});
+
+closeButton.addEventListener("click", function () {
+  parentContentMedicine.parentElement.classList.toggle("active");
+});
+
+overlay.addEventListener("click", function () {
+  closeButton.click();
 });
