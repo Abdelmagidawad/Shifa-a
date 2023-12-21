1ecb259fc53de9d9a56ef4ae12752e5160af70da
Abdelmagidawad <abdelmagidawad@gmail.com>
2023-12-22 01:01:45 +0200

    solve problem when search doc in patientProfile

--- Profile-pateint/Home/search-doctor/search-doc.js
+++ Profile-pateint/Home/search-doctor/search-doc.js
@@ -19,31 +19,41 @@ const doctorId = getCookieValue("userId");
 
 //***************************************************************** */
 // sort specialty
-// const selectElement = document.querySelector("select[name='specialty']");
-// const options = Array.from(selectElement.options);
+const selectElement = document.querySelector("select[name='specialty']");
+const options = Array.from(selectElement.options);
 
-// options.sort((a, b) => a.text.localeCompare(b.text));
+options.sort((a, b) => a.text.localeCompare(b.text));
 
-// options.forEach((option) => {
-//   selectElement.add(option);
-// });
+options.forEach((option) => {
+  selectElement.add(option);
+});
 
-//******************************************************************* */
 // Get the search input and select elements
 const searchInput = document.querySelector('input[name="namedoctor"]');
 const specialtySelect = document.querySelector('select[name="specialty"]');
 const doctorBoxes = Array.from(document.querySelectorAll(".list-doc .box"));
+//
+let noResult = document.querySelector(".no-result");
 
 //Function to filter doctors
 function filterDoctors() {
   const searchValue = searchInput.value.toLowerCase();
+  let foundResults = false;
+
   doctorBoxes.forEach((box) => {
     const name = box.querySelector("h3").textContent.toLowerCase();
     const matchesSearch = name.includes(searchValue);
     if (matchesSearch) {
       box.style.display = "block";
+      foundResults = true;
     } else {
       box.style.display = "none";
+    }
+    //
+    if (foundResults) {
+      noResult.style.display = "none";
+    } else {
+      noResult.style.display = "block";
     }
   });
 }
@@ -51,20 +61,28 @@ function filterDoctors() {
 searchInput.addEventListener("input", filterDoctors);
 
 // filter Specialty
-specialtySelect.onchange = function () {
+specialtySelect.addEventListener("change", function () {
   const specialtyValue = specialtySelect.value.toLowerCase();
+  let foundResults = false;
 
   doctorBoxes.forEach((box) => {
-    const specialty = box.querySelector("span").textContent.toLowerCase();
-    const matchesSpecialty = specialty === specialtyValue;
+    const specialty = box.querySelector(".spe span").textContent.toLowerCase();
+    const containsSpecialty = specialty.includes(specialtyValue);
 
-    if (specialtyValue === "" || matchesSpecialty) {
-      box.style.display = "block";
+    if (specialtyValue === "" || containsSpecialty) {
+      box.classList.remove("hidden");
+      foundResults = true;
     } else {
-      box.style.display = "none";
+      box.classList.add("hidden");
     }
   });
-};
+
+  if (foundResults) {
+    noResult.style.display = "none";
+  } else {
+    noResult.style.display = "block";
+  }
+});
 
 // filter Gender
 const genderSelect = document.querySelector('select[name="gender"]');
